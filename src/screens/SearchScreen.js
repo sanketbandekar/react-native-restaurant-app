f572182867ad497b9ad23204a12902bbfc0778a7
sanket <bandekarsanket03@gmail.com>
2021-12-05 01:49:27 +0530

    added api config and reusable hooks

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,27 @@
 import React, {useState} from "react";
 import { Text, View, StyleSheet } from 'react-native'
 import SearchBar from "../components/SearchBar";
+import SearchResults from "../hooks/SearchResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
 
     const [term, setTerm] = useState('')
+    const [results,errorMessage,searchApi] = SearchResults()
 
     return(
         <View style={styles.bg}>
             <SearchBar 
             term = {term} 
             onTermChange = { (newTerm) => setTerm(newTerm) }
-            onEndChange={ () => { console.log('OK') } }/>
-            <Text>{term}</Text>
+            onEndChange={ () => searchApi(term) }/>
+            { errorMessage ? <Text style={{color: 'red'}}>{errorMessage}</Text> : null }
+
+            <Text>We have found {results.length} results</Text>
+
+            <ResultsList title = "Cost Effective"/>
+            <ResultsList title = "Bit Pricer"/>
+            <ResultsList title = "Big Spender"/>
         </View>
     )
 }
